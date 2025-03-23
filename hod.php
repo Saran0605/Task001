e8caa67cf051d4a7dc9522ce2483b39e0700322c
Saran0605 <saransoundharajan05@gmail.com>
2025-03-23 11:28:00 +0530

    done

--- hod.php
+++ hod.php
@@ -604,12 +604,7 @@ $dept = $_SESSION['dept'];
                 })
             });
 
-            $( '#multiple-select-field' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-    closeOnSelect: false,
-} );
+           
     </script>
    
 </body>
